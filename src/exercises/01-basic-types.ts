9c1f045ad79bb692f1d272ace69295801b155671
EslebanPerez <eslebanperez@gmail.com>
2022-05-24 18:44:22 -0500

    Variables tipado básico en typescript

---
+++ src/exercises/01-basic-types.ts
@@ -0,0 +1,22 @@
+
+
+console.log('Hola Mundo!!!');
+/*
+    ===== Código de TypeScript =====
+*/
+let nombre = "Esleban";
+let apellido : string = "Perez";
+const trabajo= "Desarrollador Web";
+console.log(nombre);
+
+nombre = "Ezequiel";
+console.log(nombre);
+
+let isAlive : boolean = true;
+let hp : number | string = 95;
+console.log(`Is alive: ${isAlive}`);
+console.log(`health point ${hp}`);
+
+hp = "FULL";
+console.log(`health point ${hp}`);
+
